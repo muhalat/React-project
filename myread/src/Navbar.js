38b070e6f26aa5e459acbe370e32889ef2dfa93b
Muhalat <muhalat17@gmail.com>
2022-10-24 08:34:51 +0100

    Added navbar

---
+++ myread/src/Navbar.js
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <nav className="navbar">
+            <h2>The Dojo Blog</h2>
+            <div className="links">
+                {/* <Link to="/">Home</Link>
+                <Link to="/Create" style={{
+                    // color:"white",
+                    // backgroundColor: '#f1356d',
+                    // borderRadius:'8px'
+                }}>New Blog</Link> */}
+            </div>
+        </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
